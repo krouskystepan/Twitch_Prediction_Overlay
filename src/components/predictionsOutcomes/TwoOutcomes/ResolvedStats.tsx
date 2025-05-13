635322c996c2514294035766e18dccba20f6ed16
Stepan Krousky <stepan.krousky@seznam.cz>
2025-05-13 13:36:39 +0200

    enhance final stats text

--- src/components/predictionsOutcomes/TwoOutcomes/ResolvedStats.tsx
+++ src/components/predictionsOutcomes/TwoOutcomes/ResolvedStats.tsx
@@ -25,7 +25,7 @@ const ResolvedStats = ({
 }) => {
   return (
     <motion.div
-      className="absolute top-3 left-1/2 line-clamp-1 flex w-fit -translate-x-1/2 flex-col items-center justify-center text-xl font-semibold"
+      className="absolute top-2.5 left-1/2 line-clamp-1 flex w-fit -translate-x-1/2 flex-col items-center justify-center text-xl font-semibold"
       initial={{ opacity: 0, y: -20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{
@@ -34,53 +34,58 @@ const ResolvedStats = ({
         ease: 'linear',
       }}
     >
-      <Stats
-        text={formatNumberToReadableString(totalPoints)}
-        iconLeft={
-          <ChannelPoints
-            className={getCorrectTextColor({
-              isLeft: true,
-              outcomeId: outcomeOneId,
-              winningOutcomeId,
-            })}
-            size={14}
-          />
-        }
-        iconRight={
-          <ChannelPoints
-            className={getCorrectTextColor({
-              isLeft: false,
-              outcomeId: outcomeTwoId,
-              winningOutcomeId,
-            })}
-            size={14}
-          />
-        }
-      />
+      <h6 className="text-base font-semibold">Outcome Summary</h6>
 
-      <Stats
-        text={formatNumberWithSpaces(totalUsers)}
-        iconLeft={
-          <Users
-            className={getCorrectTextColor({
-              isLeft: true,
-              outcomeId: outcomeOneId,
-              winningOutcomeId,
-            })}
-            size={12}
-          />
-        }
-        iconRight={
-          <Users
-            className={getCorrectTextColor({
-              isLeft: false,
-              outcomeId: outcomeTwoId,
-              winningOutcomeId,
-            })}
-            size={12}
-          />
-        }
-      />
+      <div className="max-w-fit">
+        <Stats
+          text={formatNumberToReadableString(totalPoints)}
+          iconLeft={
+            <ChannelPoints
+              className={getCorrectTextColor({
+                isLeft: true,
+                outcomeId: outcomeOneId,
+                winningOutcomeId,
+              })}
+              size={14}
+            />
+          }
+          iconRight={
+            <ChannelPoints
+              className={getCorrectTextColor({
+                isLeft: false,
+                outcomeId: outcomeTwoId,
+                winningOutcomeId,
+              })}
+              size={14}
+            />
+          }
+          index={1}
+        />
+        <Stats
+          text={formatNumberWithSpaces(totalUsers)}
+          iconLeft={
+            <Users
+              className={getCorrectTextColor({
+                isLeft: true,
+                outcomeId: outcomeOneId,
+                winningOutcomeId,
+              })}
+              size={12}
+            />
+          }
+          iconRight={
+            <Users
+              className={getCorrectTextColor({
+                isLeft: false,
+                outcomeId: outcomeTwoId,
+                winningOutcomeId,
+              })}
+              size={12}
+            />
+          }
+          index={2}
+        />
+      </div>
     </motion.div>
   )
 }
@@ -89,17 +94,28 @@ const Stats = ({
   text,
   iconLeft,
   iconRight,
+  index,
 }: {
   text: string
   iconLeft: ReactNode
   iconRight: ReactNode
+  index: number
 }) => {
   return (
-    <p className="flex w-full items-center justify-between gap-2 text-sm font-semibold">
+    <motion.p
+      className="flex w-full items-center justify-between gap-2 text-sm"
+      initial={{ opacity: 0, y: -20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        delay: 1 + index * 0.3,
+        duration: 0.2,
+        ease: 'linear',
+      }}
+    >
       {iconLeft}
       {text}
       {iconRight}
-    </p>
+    </motion.p>
   )
 }
 
